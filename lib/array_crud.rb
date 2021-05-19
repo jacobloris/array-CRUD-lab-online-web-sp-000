908141f8b0eb634cdeed9940df0150f12452a99e
Jacob Loris <jacobloris@outlook.com>
2021-05-19 01:02:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   [husky, lab, aussie, shepherd]
+   << [greyhound]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  unshift 
 end
 
 def remove_element_from_end_of_array(array)
